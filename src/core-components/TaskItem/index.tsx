847b77fc3194839657b3826ce8ae191926be8ed6
Luiz Togni <contato.togni@gmail.com>
2025-08-27 20:52:58 -0300

    feat: add create and update in component

--- src/core-components/TaskItem/index.tsx
+++ src/core-components/TaskItem/index.tsx
@@ -1,3 +1,4 @@
+import { cx } from 'class-variance-authority';
 import React from 'react';
 import CheckIcon from '../../assets/icons/check.svg?react';
 import PencilIcon from '../../assets/icons/pencil.svg?react';
@@ -8,9 +9,19 @@ import Card from '../../components/Card';
 import InputCheckbox from '../../components/InputCheckbox';
 import InputText from '../../components/InputText';
 import Text from '../../components/Text';
+import useTask from '../../hooks/use-task';
+import { TaskState, type Task } from '../../models/task';
 
-export default function TaskItem() {
-  const [isEditing, setIsEditing] = React.useState(false);
+interface TaskItemProps {
+  task: Task;
+}
+
+export default function TaskItem({ task }: TaskItemProps) {
+  const [isEditing, setIsEditing] = React.useState(
+    task?.state === TaskState.Creating
+  );
+  const [taskTitle, setTaskTitle] = React.useState(task.title || '');
+  const { updateTask } = useTask();
 
   function handleEditTask() {
     setIsEditing(true);
@@ -20,12 +31,31 @@ export default function TaskItem() {
     setIsEditing(false);
   }
 
+  function handleChangeTaskTitle(e: React.ChangeEvent<HTMLInputElement>) {
+    setTaskTitle(e.target.value || '');
+  }
+
+  function handleSaveTask(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setIsEditing(false);
+    updateTask(task.id, { title: taskTitle });
+  }
+
   return (
-    <Card size="md" className="flex items-center gap-4">
+    <Card size="md">
       {!isEditing ? (
-        <>
-          <InputCheckbox />
-          <Text className="flex-1">Fazer compras da semana</Text>
+        <div className="flex items-center gap-4">
+          <InputCheckbox
+            value={task?.concluded?.toString()}
+            checked={task?.concluded}
+          />
+          <Text
+            className={cx('flex-1', {
+              'line-through': task?.concluded,
+            })}
+          >
+            {task?.title}
+          </Text>
           <div className="flex gap-1">
             <ButtonIcon icon={TrashIcon} variant="tertiary" />
             <ButtonIcon
@@ -34,19 +64,25 @@ export default function TaskItem() {
               onClick={handleEditTask}
             />
           </div>
-        </>
+        </div>
       ) : (
-        <>
-          <InputText className="flex-1" />
+        <form onSubmit={handleSaveTask} className="flex items-center gap-4">
+          <InputText
+            value={taskTitle}
+            className="flex-1"
+            onChange={handleChangeTaskTitle}
+            required
+            autoFocus
+          />
           <div className="flex gap-1">
             <ButtonIcon
               icon={XIcon}
               variant="secondary"
               onClick={handleExitEditTask}
             />
-            <ButtonIcon icon={CheckIcon} variant="primary" />
+            <ButtonIcon type="submit" icon={CheckIcon} variant="primary" />
           </div>
-        </>
+        </form>
       )}
     </Card>
   );
