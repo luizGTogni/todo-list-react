24c01d0d6eedf56708f6fc1739c24acf3f814d87
Luiz Togni <contato.togni@gmail.com>
2025-08-27 20:03:46 -0300

    feat: add page home

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,10 +1,15 @@
 import Container from '../components/Container';
-import Text from '../components/Text';
+import TaskList from '../core-components/TaskList';
+import TasksSummary from '../core-components/TasksSummary';
 
 export default function PageHome() {
   return (
-    <Container>
-      <Text>PAGINA HOME!</Text>
+    <Container as="article" className="space-y-3">
+      <header className="flex items-center justify-between">
+        <TasksSummary />
+      </header>
+
+      <TaskList />
     </Container>
   );
 }
