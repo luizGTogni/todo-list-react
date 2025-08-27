ad97d1e49fe9debe2f2c7edf1579d7492b5209c0
Luiz Togni <contato.togni@gmail.com>
2025-08-27 10:09:14 -0300

    refactor: change structure button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,41 @@
+import { type VariantProps } from 'class-variance-authority';
+import Icon from '../Icon';
+import Text from '../Text';
+import {
+  buttonIconVariants,
+  buttonTextVariants,
+  buttonVariants,
+} from './variants';
+
+interface ButtonProps
+  extends Omit<React.ComponentProps<'button'>, 'size' | 'disabled'>,
+    VariantProps<typeof buttonVariants> {
+  icon?: React.ComponentProps<typeof Icon>['svg'];
+}
+
+export default function Button({
+  variant,
+  size,
+  disabled,
+  className,
+  children,
+  icon: IconComponent,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={buttonVariants({ variant, size, disabled, className })}
+      {...props}
+    >
+      {IconComponent && (
+        <Icon
+          svg={IconComponent}
+          className={buttonIconVariants({ variant, size })}
+        />
+      )}
+      <Text variant="body-md-bold" className={buttonTextVariants({ variant })}>
+        {children}
+      </Text>
+    </button>
+  );
+}
