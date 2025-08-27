7ab2f9329dce821f3ab70c2124f5c25389e5de1d
Luiz Togni <contato.togni@gmail.com>
2025-08-27 20:52:19 -0300

    feat: add create and update task function

---
+++ src/hooks/use-task.ts
@@ -0,0 +1,32 @@
+import useLocalStorage from 'use-local-storage';
+import { TASK_KEY, TaskState, type Task } from '../models/task';
+
+export default function useTask() {
+  const [tasks, setTasks] = useLocalStorage<Task[]>(TASK_KEY, []);
+
+  function prepareTask() {
+    setTasks([
+      ...tasks,
+      {
+        id: Math.random().toString(36).substring(2, 9),
+        title: '',
+        state: TaskState.Creating,
+      },
+    ]);
+  }
+
+  function updateTask(id: string, payload: { title: Task['title'] }) {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id
+          ? { ...task, state: TaskState.Created, ...payload }
+          : task
+      )
+    );
+  }
+
+  return {
+    prepareTask,
+    updateTask,
+  };
+}
