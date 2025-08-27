0b6d1df568f6b8322a111026e00fd5039ac8cee5
Luiz Togni <contato.togni@gmail.com>
2025-08-27 11:13:03 -0300

    feat: add skeleton in the input checkbox

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -8,23 +8,35 @@ import {
 } from './variants';
 import Icon from '../Icon';
 import CheckIcon from '../../assets/icons/check.svg?react';
+import Skeleton from '../Skeleton';
 
 interface InputCheckboxProps
   extends Omit<React.ComponentProps<'input'>, 'size' | 'disabled'>,
-    VariantProps<typeof inputCheckboxVariants> {}
+    VariantProps<typeof inputCheckboxVariants> {
+  loading?: boolean;
+}
 
 export default function InputCheckbox({
+  loading,
+  variant,
   size,
   disabled,
   className,
   ...props
 }: InputCheckboxProps) {
+  if (loading) {
+    <Skeleton
+      rounded="sm"
+      className={inputCheckboxVariants({ variant: 'none', size })}
+    />;
+  }
+
   return (
     <label className={inputCheckboxWrapperVariants({ className })}>
       <input
         type="checkbox"
         className={cx(
-          inputCheckboxVariants({ size, disabled }),
+          inputCheckboxVariants({ variant, size, disabled }),
           textVariants(),
           className
         )}
--- src/components/InputCheckbox/variants.tsx
+++ src/components/InputCheckbox/variants.tsx
@@ -7,13 +7,20 @@ export const inputCheckboxWrapperVariants = cva(
 export const inputCheckboxVariants = cva(
   `
   appearance-none peer flex items-center justify-center cursor-pointer
-  border-2 border-solid transition overflow-hidden
-  border-green-base hover:border-green-dark hover:bg-green-dark/20
-  checked:border-green-base checked:bg-green-base
-  group-hover:checked:border-green-dark group-hover:checked:bg-green-dark
+   transition overflow-hidden
+  
 `,
   {
     variants: {
+      variant: {
+        none: '',
+        default: `
+          border-2 border-solid
+          border-green-base hover:border-green-dark hover:bg-green-dark/2
+          checked:border-green-base checked:bg-green-base
+          group-hover:checked:border-green-dark group-hover:checked:bg-green-dark
+        `,
+      },
       size: {
         md: 'w-5 h-5 rounded-sm',
       },
@@ -22,6 +29,7 @@ export const inputCheckboxVariants = cva(
       },
     },
     defaultVariants: {
+      variant: 'default',
       size: 'md',
       disabled: false,
     },
