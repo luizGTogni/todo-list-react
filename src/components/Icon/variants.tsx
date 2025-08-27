8969b4aa5019cd9c0fb17a6fc66d469af4ac7969
Luiz Togni <contato.togni@gmail.com>
2025-08-27 10:08:32 -0300

    refactor: change structure icon component

---
+++ src/components/Icon/variants.tsx
@@ -0,0 +1,13 @@
+import { cva } from 'class-variance-authority';
+
+export const iconVariants = cva('', {
+  variants: {
+    animate: {
+      false: '',
+      true: 'animate-spin',
+    },
+  },
+  defaultVariants: {
+    animate: false,
+  },
+});
