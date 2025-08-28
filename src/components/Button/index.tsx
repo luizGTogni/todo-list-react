0f910bcb45752f0ff9b9ed44b224efa6a04e2c9b
Luiz Togni <contato.togni@gmail.com>
2025-08-27 22:32:19 -0300

    feat: add simulated request api with delay

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,5 @@
 import { type VariantProps } from 'class-variance-authority';
+import SpinnerIcon from '../../assets/icons/spinner.svg?react';
 import Icon from '../Icon';
 import Text from '../Text';
 import {
@@ -11,6 +12,7 @@ interface ButtonProps
   extends Omit<React.ComponentProps<'button'>, 'size' | 'disabled'>,
     VariantProps<typeof buttonVariants> {
   icon?: React.ComponentProps<typeof Icon>['svg'];
+  handling?: boolean;
 }
 
 export default function Button({
@@ -19,17 +21,25 @@ export default function Button({
   disabled,
   className,
   children,
-  icon: IconComponent,
+  handling,
+  icon,
   ...props
 }: ButtonProps) {
   return (
     <button
-      className={buttonVariants({ variant, size, disabled, className })}
+      className={buttonVariants({
+        variant,
+        size,
+        disabled,
+        className,
+        handling,
+      })}
       {...props}
     >
-      {IconComponent && (
+      {icon && (
         <Icon
-          svg={IconComponent}
+          svg={handling ? SpinnerIcon : icon}
+          animate={handling}
           className={buttonIconVariants({ variant, size })}
         />
       )}
--- src/hooks/use-tasks.ts
+++ src/hooks/use-tasks.ts
@@ -1,12 +1,30 @@
+import React from 'react';
 import useLocalStorage from 'use-local-storage';
+import delay from '../helpers/utils';
 import { TASK_KEY, TaskState, type Task } from '../models/task';
 
 export default function useTasks() {
-  const [tasks] = useLocalStorage<Task[]>(TASK_KEY, []);
+  const [tasksData] = useLocalStorage<Task[]>(TASK_KEY, []);
+  const [tasks, setTasks] = React.useState<Task[]>([]);
+  const [isLoadingTasks, setIsLoadingTasks] = React.useState(true);
+
+  const fetchTasks = React.useCallback(async () => {
+    if (isLoadingTasks) {
+      await delay(200);
+      setIsLoadingTasks(false);
+    }
+
+    setTasks(tasksData);
+  }, [tasksData, isLoadingTasks]);
+
+  React.useEffect(() => {
+    fetchTasks();
+  }, [fetchTasks, tasksData]);
 
   return {
     tasks,
     tasksCount: tasks.filter((task) => task.state === TaskState.Created).length,
     concludedTasksCount: tasks.filter((task) => task.concluded).length,
+    isLoadingTasks,
   };
 }
